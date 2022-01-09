01bc8a03eba317b60d65632bbdee4e35d561fe91
DevilsHand <gabriel88santos@gmail.com>
2022-01-09 00:28:35 -0300

    colocando apenas uma entrada

--- src/main/java/grafico/janelas/FrameJogador.java
+++ src/main/java/grafico/janelas/FrameJogador.java
@@ -2,8 +2,6 @@ package main.java.grafico.janelas;
 
 import static main.java.calculator.main.CONTROLADOR;
 
-import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,8 +9,6 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.tools.DiagnosticCollector;
 
 import main.java.calculator.jogo.Jogador;
 
@@ -25,7 +21,6 @@ public class FrameJogador extends JPanel implements ActionListener {
 	private final JPanel pvEBotoes = new JPanel();
 	private final JLabel displayNome;
 	private final JLabel displayPV;
-	private final JTextField entradaValor;
 	private final JButton adicionar = new JButton("< + >");
 	private final JButton subtrair = new JButton("< - >");
 	private final JButton desfazer = new JButton("Desfazer");
@@ -39,16 +34,15 @@ public class FrameJogador extends JPanel implements ActionListener {
 		// inicia os displays
 		displayNome = new JLabel(nome);
 		displayPV = new JLabel(pontosVida.toString());
-		entradaValor = new JTextField();
 
 		iniciarJanela();
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (! entradaValor.getText().matches(REGEX)) {
+		if (! CONTROLADOR.acessarValor().matches(REGEX)) {
 			return;// Não executa nada caso sejam inseridos caracteres que não sejam numeros
 		}
-		Long valor = Long.parseLong(entradaValor.getText());
+		Long valor = Long.parseLong(CONTROLADOR.acessarValor());
 		//Variavel só pq eu não gosto de colocar expressões
 		switch (e.getActionCommand()) {
 		case "soma":
@@ -61,7 +55,7 @@ public class FrameJogador extends JPanel implements ActionListener {
 			jogador.desfazer();
 			break;
 		}
-		entradaValor.setText("0");
+		CONTROLADOR.colocarValor("0");
 		desfazer.setEnabled(jogador.checarHistorico());
 		displayPV.setText(jogador.acessarVida().toString());
 		revalidate();
@@ -84,11 +78,9 @@ public class FrameJogador extends JPanel implements ActionListener {
 		adicionar.setAlignmentX(LEFT_ALIGNMENT);
 		subtrair.setAlignmentX(LEFT_ALIGNMENT);
 		desfazer.setAlignmentX(CENTER_ALIGNMENT);
-		entradaValor.setText("0");
 		// adiciona botoes e pvs aos frames
 		titulo.add(displayNome);
 		titulo.add(displayPV);
-		titulo.add(entradaValor);
 		pvEBotoes.add(adicionar);
 		pvEBotoes.add(subtrair);
 		pvEBotoes.add(desfazer);
--- src/main/java/grafico/janelas/JanelaPrincipal.java
+++ src/main/java/grafico/janelas/JanelaPrincipal.java
@@ -1,12 +1,16 @@
 package main.java.grafico.janelas;
 
 import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import main.java.calculator.jogo.Jogador;
 
 public class JanelaPrincipal extends JFrame{
+	private static final JTextField ENTRADA_VALOR = new JTextField();
 	private JPanel painelAtivo;
 	
 	public JanelaPrincipal() {
@@ -27,11 +31,20 @@ public class JanelaPrincipal extends JFrame{
 		limparMenu();
 		//cria os jogadores
 		JPanel jogadorUm = new FrameJogador(new Jogador("Jogador 1"));
-		JPanel jogadorDois = new FrameJogador(new Jogador("Jogador 1"));
+		JPanel jogadorDois = new FrameJogador(new Jogador("Jogador 2"));
+		JPanel campoJogadores = new JPanel();
+		JPanel campoEntrada = new JPanel();
+		//posiciona os campos
+		ENTRADA_VALOR.setText("0");
+		campoJogadores.setLayout(new BoxLayout(campoJogadores, BoxLayout.LINE_AXIS));
+		campoEntrada.add(ENTRADA_VALOR);
+		campoJogadores.add(jogadorUm);
+		campoJogadores.add(jogadorDois);
 		//limpa o painel e adiciona os jogadores
 		painelAtivo = new JPanel();
-		painelAtivo.add(jogadorUm);
-		painelAtivo.add(jogadorDois);
+		painelAtivo.setLayout(new BoxLayout(painelAtivo, BoxLayout.PAGE_AXIS));
+		painelAtivo.add(campoJogadores);
+		painelAtivo.add(campoEntrada);
 		add(painelAtivo);
 		revalidate();
 		pack();
@@ -45,6 +58,13 @@ public class JanelaPrincipal extends JFrame{
 	}
 	public void ajuda() {
 		limparMenu();
+	}
+	public String acessarValor() {
+		return ENTRADA_VALOR.getText();
+	}
+	public void colocarValor(String valor) {
+		ENTRADA_VALOR.setText(valor);
+		
 		
 	}
 }
